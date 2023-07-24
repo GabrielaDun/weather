6145b12404e1c4b9d9bcdb475d651729127d57ba
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-07-24 11:14:42 +0200

    Added city, icon and tempetarture display

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,21 +1,33 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 const WeatherBox = () => {
 
+  const [weatherData, setWeatherData] = useState('London');
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const handleCityChange = useCallback((city) => {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=YOUR_API_KEY&units=metric`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=76efa90774fe579ae9d46234658f7612&units=metric`)
     .then(res => res.json())
       .then(data => {
         console.log(data, '<-data, city ->', city);
+        const weatherData = {
+          city: data.name,
+          temp: data.main.temp,
+          icon: data.weather[0].icon,
+          description: data.weather[0].main
+        };
+        setWeatherData(weatherData);
+
    });
   });
+  
   return (
     <section>
       <PickCity action={handleCityChange}/>
-      <WeatherSummary />
+      <WeatherSummary data={weatherData}/>
       <Loader />
     </section>
   )
